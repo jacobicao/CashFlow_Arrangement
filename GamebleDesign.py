f195ab0ce4c7cecb0c9ed356327024b0d422de92
Jacob Cao <caozj1017@hotmail.com>
2018-01-19 11:35:15 +0800

    adjust parameter

--- GamebleDesign.py
+++ GamebleDesign.py
@@ -4,7 +4,6 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
-import math
 
 # 模拟时间区间长度
 t = 100
@@ -18,9 +17,9 @@ fr = 0.04
 # 人头补贴
 ah = 1.0
 # 模拟次数
-pn = 9
+pn = 10
 # 复投比例
-ir = 0.5
+ir = 0.65
 # 加速券票面年化利率
 ar = 0.0365
 
@@ -120,13 +119,13 @@ def simulation_loop(ps=0.125):
     ar_r = np.linspace(ar, 0.04, pn)
     for p_ar in ar_r:
         money_pool_radio = np.zeros(t)
-        for k in range(10):
+        for k in range(20):
             money_pool_radio += inner_loop(t, ps, ir, n1, n2, fund, fr, p_ar)
-        set_radio_ts[:, i] = money_pool_radio/10
+        set_radio_ts[:, i] = money_pool_radio/20
         i = i + 1
 
     # 作图：在获奖率 p 下奖金池占比的时间序列
-    # plot_money_pool_radio_ts(pp, set_radio_ts)
+    # plot_money_pool_radio_ts(ar_r, set_radio_ts)
     # 作图：在获奖率 p 下奖金池日净流入的均值标准差
     # plot_money_pool_mean_std(pp, set_radio_ts)
     # 平衡试算
@@ -177,12 +176,12 @@ def plot_money_pool_mean_std(pp, money_pool_radio):
     qq_mean = np.zeros(len(pp))
     for ii in range(len(pp)):
         qq_mean[ii] = np.polyfit(x, money_pool_radio[:, ii], deg=1)[0]
-    plt.bar(pp, qq_mean / n, width)
+    plt.bar(pp, qq_mean / n2, width)
     plt.ylabel('mean')
     plt.xlabel('p')
     plt.title('Money pool daily mean and std per custom')
     plt.subplot(212)
-    plt.bar(pp, qq.std(axis=0) / n, width)
+    plt.bar(pp, qq.std(axis=0) / n2, width)
     plt.ylabel('std')
     plt.xlabel('p')
     plt.show()
@@ -204,29 +203,28 @@ def plot_money_pool_radio_ts(pp, money_pool_radio):
         reg = np.polyfit(x, money_pool_radio[:, ii], deg=1)
         ry = np.polyval(reg, x)
         plt.plot(ry, 'r-')
-        plt.ylabel('p = %3.2f' % pp[ii])
-        # plt.ylim([0, 1])
+        plt.ylabel('%3.2f%%' % (pp[ii]*100))
 
     plt.subplot(nn*100+11)
     # plt.title('Money Pool Radio of each day')
     # plt.title('Money Pool of each day')
-    plt.title('Allowance of each day')
+    plt.title('Allowance + Pool (Recast=%.2f)' % ir)
     plt.show()
 
 
 # 作图：用户本金与最终受益对比
 def plot_fund_user_account(fund, user_account):
-    zz = np.zeros((N, 2))
+    zz = np.zeros((n2, 2))
     zz[:, 1] = fund.T
     zz[:, 0] = user_account[-1, :].T
     zz = zz[np.lexsort(-zz.T)]
     # 最后一天各用户收益情况
     plt.plot(zz[:, 0])
-    plt.title('Profit of each custom(p = %5.2f)' % p)
+    plt.title('Profit of each custom(p = %5.2f)' % ir)
     plt.show()
     # 用户初始金额
     plt.plot(zz[:, 1])
-    plt.title('Initial Value of each custom(p = %5.2f)' % p)
+    plt.title('Initial Value of each custom(p = %5.2f)' % ir)
     plt.show()
 
 
@@ -242,7 +240,7 @@ def plot_allowance_surface(pp, money_pool_radio):
     ax.set_ylabel('Time')
     ax.set_zlabel('Net')
     ax.set_title('Allowance + Pool (Recast=%.2f)' % ir)
-    ax.view_init(20, -15)
+    ax.view_init(15, 60)
     fig.colorbar(surf, shrink=0.5, aspect=5)
     plt.show()
 
