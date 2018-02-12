912e1f8b9d23e14507e95287b8e33a41d6095b19
Jacob Cao <caozj1017@hotmail.com>
2018-02-13 02:59:19 +0800

    read data from csv

--- CreditCard/run.py
+++ CreditCard/run.py
@@ -8,18 +8,18 @@ from Card_pad import Card_pad
 if __name__ == "__main__":
     pad = Card_pad()
     with open('card.csv') as fc:
+        fc.readline()
         reader = csv.reader(fc)
         for row in reader:
             Credit_card(pad,row[0],int(row[1]),int(row[2]),int(row[3]),0)
 
-    rng = pd.date_range('2018-2-1','2018-12-31')
-    pad.get_card('中行J').consume(pd.datetime(2018,1,20),13000)
-    pad.get_card('平安J').consume(pd.datetime(2018,1,20),27000)
-    pad.get_card('中信J').consume(pd.datetime(2018,2,1),13000)
-    pad.get_card('农行J').consume(pd.datetime(2018,2,1),5300)
-    pad.get_card('建设Q').consume(pd.datetime(2018,1,20),39000)
-    pad.get_card('中行Q').consume(pd.datetime(2018,1,20),25000)
+    with open('now.csv') as fn:
+        fn.readline()
+        reader = csv.reader(fn)
+        for w in reader:
+            pad.get_card(w[0]).consume(pd.datetime(int(w[1]),int(w[2]),int(w[3])),int(w[4]))
 
+    rng = pd.date_range('2018-2-1','2018-12-31')
     for t in rng:
         pad.check_repay(t)
 
