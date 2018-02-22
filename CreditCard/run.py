a26b8fb0fa7bd719171484c68ae52a4185595412
Jacob Cao <caozj1017@hotmail.com>
2018-02-23 02:46:19 +0800

    整理代码，分割主函数逻辑

--- CreditCard/run.py
+++ CreditCard/run.py
@@ -5,33 +5,40 @@ import pandas as pd
 from Credit_card import Credit_card
 from Card_pad import Card_pad, logger
 
-if __name__ == "__main__":
-    pad = Card_pad()
-    with open('card.csv') as fc:
+def get_ic():
+    iic = dict()
+    with open('data/income.csv') as fi:
+        fi.readline()
+        reader = csv.reader(fi)
+        for w in reader:
+            t = pd.datetime(int(w[0]),int(w[1]),int(w[2]))
+            iic[t] = int(w[3])
+    return iic
+
+def init_pad(pad):
+    with open('data/card.csv') as fc:
         fc.readline()
         reader = csv.reader(fc)
         for row in reader:
             Credit_card(pad,row[0],int(row[1]),int(row[2]),int(row[3]))
 
-    with open('now.csv') as fn:
+    with open('data/now.csv') as fn:
         fn.readline()
         reader = csv.reader(fn)
         for w in reader:
             t = pd.datetime(int(w[1]),int(w[2]),int(w[3]))
             pad.get_card(w[0]).consume(t,int(w[4]))
 
-    iic = dict()
-    with open('income.csv') as fi:
-        fi.readline()
-        reader = csv.reader(fi)
-        for w in reader:
-            t = pd.datetime(int(w[0]),int(w[1]),int(w[2]))
-            iic[t] = int(w[3])
-
+def main():
+    pad = Card_pad()
+    init_pad(pad)
+    iic = get_ic()
     rng = pd.date_range('2018-2-1','2018-12-31')
     for t in rng:
         if t in iic:
             pad.set_income(iic[t])
         pad.check_repay(t)
-
     logger.info('\n%s: 当前总负债还有 %.f\n'%(t.date(),pad.get_total_debt()))
+
+if __name__ == "__main__":
+    main()
