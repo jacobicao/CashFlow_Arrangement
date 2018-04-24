f21ac81aeb3d8c4187fed44f9ad88a31fe6ada94
Zhijie Cao <caozj1017@hotmail.com>
2018-04-24 20:16:26 +0800

    1.整合api，简化调用脚本 2.中文交互提示

--- CreditCard/MyApi.py
+++ CreditCard/MyApi.py
@@ -3,47 +3,80 @@ from Credit_card import Credit_card
 from Card_pad import Card_pad, logger
 import pandas as pd
 
-
-def find_user_id(s):
-	return find_user(s)
-
-
+# 收入类
 def get_ic(uid):
     iic = dict()
     for v in find_income(uid):
         iic[v[0]] = int(v[1])
     return iic
 
+# 用户类
+def input_user_name():
+    uid = find_user(input('输入用户名:'))
+    while(uid == 0):
+        print('用户名不存在!')
+        uid = find_user(input('输入用户名:'))
+    return uid
+    
+# 卡类
 def card_list(uid):
+    l = []
+    print('='*20)
     for v in find_card(uid):
-        print(v[0],':',v[1])
+        if v[1] != '房贷':
+            print('%2d:%6s'%(v[0],v[1]))
+            l.append(v[0])
+    print('='*20)
+    return l
 
+# 账单类
 def debt_list(uid):
     l = []
+    print('='*20)
     for v in find_debt(uid):
         if v[1] == '房贷':
             continue
-        print('%d:%s consume %d on %s'%(v[4],v[1],v[3],v[2]))
+        print('%2d:%s 消费 %d 在 %s'%(v[4],v[1],v[3],v[2]))
         l.append(v[4])
+    print('='*20)
     return l
 
-def delete_one_debt(uid,did):
-    delete_debt(uid,did)
+def add_one_debt(uid):
+    l = card_list(uid)
+    cid = input('哪张卡?')
+    if not int(cid) in l:
+        print('输入错误')
+        return
+    dt = input('什么时候(YYYY-MM-DD)?')
+    num = input('刷了多少?')
+    while(not is_float(num)):
+        print('请输入数字')
+        num = input('刷了多少?')
+    try:
+        add_debt(uid,cid,pd.to_datetime(dt).date(),num)
+    except Exception as e:
+        print('输入错误:',e)
+    except:
+        print('输入错误!')
+    else:
+        print('添加成功!')
 
+def delete_one_debt(uid):
+    l = debt_list(uid)
+    did = int(input('哪一条?'))
+    if not did in l:
+        print('输入错误!')
+        return
+    delete_debt(uid,did)
+    print('删除成功!')
+    
+# 卡包类
 def init_pad(pad,uid):
     for v in find_card(uid):
         Credit_card(pad,v[0],v[1],int(v[2]),int(v[3]),int(v[4]))
     for v in find_debt(uid):
         pad.get_card(v[0]).consume(v[2],int(v[3]))
     return
-
-    
-def add_ont_debt(u,c,t,n):
-    add_debt(u,c,pd.to_datetime(t).date(),n)
-
-def get_pad():
-    return Card_pad()
-
 
 def cal_plan(pad,iic,dt):
     rng = pd.date_range(dt,'2018-12-31')
@@ -58,3 +91,25 @@ def cal_plan(pad,iic,dt):
     plan.index = pd.to_datetime(plan.date)
     del plan['date']
     return plan
+
+def show_plan(uid):
+    dt = '2018-01-01'
+    pad = Card_pad()
+    init_pad(pad,uid)
+    iic = get_ic(uid)
+    plan = cal_plan(pad,iic,dt)
+    plan.to_csv('log/Cash_out_plan.csv',float_format='%d')
+    print('='*20)
+    print(plan)
+    print('='*20)
+    
+# 工具类
+def is_float(str):
+    try:     
+        f = float(str) 
+    except ValueError:     
+        return False
+    return True
+
+proc = dict({'1':show_plan,'2':debt_list,'3':add_one_debt,'4':delete_one_debt})
+word = '\n(1)查看计划\n(2)查看当前账单\n(3)增加一条刷卡记录\n(4)删除一条刷卡记录\n(e)退出\n请输入:'
--- CreditCard/run.py
+++ CreditCard/run.py
@@ -1,74 +1,23 @@
 #!/usr/bin/env python
 # encoding: utf-8
 import MyApi
+import time
 
-def input_user_name():
-    uid = MyApi.find_user_id(input('Input user name:'))
-    while(uid == 0):
-        print('User name does not exist!')
-        uid = MyApi.find_user_id(input('Input user name:'))
-    return uid
 
-def show_plan(uid):
-    dt = '2018-02-01'
-    pad = MyApi.get_pad()
-    MyApi.init_pad(pad,uid)
-    iic = MyApi.get_ic(uid)
-    plan = MyApi.cal_plan(pad,iic,dt)
-    plan.to_csv('log/Cash_out_plan.csv',float_format='%d')
-    print(plan)
-
-def show_debt(uid):
-    MyApi.debt_list(uid)
-    
-def is_float(str):
-    try:     
-        f = float(str) 
-    except ValueError:     
-        return False
-    return True
-
-def add_debt(uid):
-    MyApi.card_list(uid)
-    cid = input('Which card?')
-    dt = input('When happen?')
-    num = input('How many?')
-    while(not is_float(num)):
-        print('Not a number')
-        num = input('How many?')
-    try:
-        MyApi.add_ont_debt(uid,cid,dt,num)
-    except Exception as e:
-        print('Error input:',e)
-    except:
-        print('Error input!')
-    else:
-        print('Success!')
-
-def delete_debt(uid):
-    l = MyApi.debt_list(uid)
-    did = int(input('Which debt?'))
-    if not did in l:
-        print('Error input!')
-        return
-    MyApi.delete_one_debt(uid,did)
-    print('Success!')
-
-proc = dict({'1':show_plan,'2':show_debt,'3':add_debt,'4':delete_debt})
-word = '\n(1)Show plan\n(2)Show debt\n(3)Add debt\n(4)Delete debt\n(e)xit\nPlease input:'
 
 def main():
-    uid = 1#input_user_name()
+    uid = MyApi.input_user_name()
     B = False
     while(not B):
-        p = input(word)
-        if p in proc.keys():
-            proc[p](uid)
+        p = input(MyApi.word)
+        if p in MyApi.proc.keys():
+            MyApi.proc[p](uid)
         elif p is 'e':
             B = True
-            print('Bye bye!\n')
+            print('拜拜!\n')
         else:
-            print('Error input!\n')
+            print('输入不对，再来一次!')
+            time.sleep(3)
 
 
 if __name__ == "__main__":
