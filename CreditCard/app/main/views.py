33cbc53827b635ed4590ad6be331701d23f4657d
Jacob Cao <caozj1017@hotmail.com>
2018-06-27 19:01:08 +0800

    增加一个引用包

--- CreditCard/app/main/views.py
+++ CreditCard/app/main/views.py
@@ -4,8 +4,8 @@ from flask import render_template, redirect, url_for, abort, flash, request,\
 from flask_login import login_required, current_user
 from flask_sqlalchemy import get_debug_queries
 from . import main
-
+import time
 
 @main.route('/')
 def index():
-    return '<h1>Hello</h1>'
+    return '<h1>Hello, %s</h1>'%time.ctime()
